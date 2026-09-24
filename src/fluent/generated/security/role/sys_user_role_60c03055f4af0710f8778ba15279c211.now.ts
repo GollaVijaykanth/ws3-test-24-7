8b96e62af969dd6413f4cba550c9a9b9e84e5b28
System Administrator <admin@example.com>
2026-09-24 19:16:58 +0530

    test All

--- src/fluent/generated/security/role/sys_user_role_60c03055f4af0710f8778ba15279c211.now.ts
+++ src/fluent/generated/security/role/sys_user_role_60c03055f4af0710f8778ba15279c211.now.ts
@@ -2,6 +2,6 @@ import { Role } from '@servicenow/sdk/core'
 
 Role({
     name: 'x_snc_ws3test24_0.admin',
-    description: 'Default admin role users',
+    description: 'Default admin role',
     federatedId: 'MkVIahrAHmDGbQaa7hbcblAxrJtk5P4UOfK+DaZO2ak=',
 })
